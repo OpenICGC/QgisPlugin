4ae9b54738b87a516a834aeebfd14d1eb7cc9ad0
a.adell <albert.adell@icgc.cat>
2022-05-27 10:08:04 +0200

    Added compile resources windows script (partially interactive)

--- compile_resources.bat
+++ compile_resources.bat
@@ -1,1 +1,9 @@
-rem Compiling language files (interactively)
+rem Compiling plugin and dialog resources
+pyrcc5 -o resources_rc.py resources.qrc
+pyrcc5 -o qlib3\base\resources_rc.py qlib3\base\resources.qrc
+pyrcc5 -o qlib3\geofinderdialog\resources_rc.py qlib3\geofinderdialog\resources.qrc
+pyrcc5 -o qlib3\photosearchselectiondialog\resources_rc.py qlib3\photosearchselectiondialog\resources.qrc
+
+rem Compiling language files (ts -> qm interactively)
+linguist i18n\openicgc_ca.ts
+linguist i18n\openicgc_es.ts
